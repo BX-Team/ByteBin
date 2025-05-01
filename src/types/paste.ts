5258aef6947b0478a56fd8547d7add66c99a3e7a
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-05-02 01:00:48 +0300

    fix: remove prisma from types

--- src/types/paste.ts
+++ src/types/paste.ts
@@ -1,8 +1,34 @@
-import { Paste as PrismaPaste } from '@prisma/client';
-
-export type Paste = PrismaPaste & {
+export type Paste = {
   /**
    * The paste's ID.
    */
   key: string;
+  /**
+   * The paste's content.
+   */
+  content: string;
+  /**
+   * The paste's file extension.
+   */
+  ext: string;
+  /**
+   * The paste's programming language.
+   */
+  language: string;
+  /**
+   * The paste's expiration date.
+   */
+  expires_at: string | null;
+  /**
+   * The paste's size in bytes.
+   */
+  size: number;
+  /**
+   * The paste's creation timestamp.
+   */
+  timestamp: string;
+  /**
+   * The paste's view count.
+   */
+  views: number;
 };
