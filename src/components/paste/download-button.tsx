83dc65cbdf0d4877dabb0af2ab111f7496ef2f66
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-05-03 17:34:11 +0300

    fix: download file extensions

--- src/components/paste/download-button.tsx
+++ src/components/paste/download-button.tsx
@@ -3,7 +3,14 @@
 import { Paste } from '@/types/paste';
 import { Button } from '@/components/button';
 import { downloadFile } from '@/common/utils/browser.util';
+import { fileExtensions } from '@/common/utils/lang.util';
 
 export function DownloadPasteButton({ paste }: { paste: Paste }) {
-  return <Button onClick={() => downloadFile(`${paste.id}`, paste.content)}>Download</Button>;
+  const getFileName = (paste: Paste) => {
+    if (paste.language.toLowerCase() === 'dockerfile') return 'Dockerfile';
+    const extension = fileExtensions[paste.language.toLowerCase()] || 'txt';
+    return `${paste.id}.${extension}`;
+  };
+
+  return <Button onClick={() => downloadFile(getFileName(paste), paste.content)}>Download</Button>;
 }
